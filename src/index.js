be41a5a5fd58e455dabf3f9aea9dfc2ff5991073
lil.void <tsraduu@gmail.com>
2024-02-04 20:06:32 +0000

    started working on the new page and stoped due to late day and taking a break

--- src/index.js
+++ src/index.js
@@ -2,72 +2,38 @@ import "./style.css";
 import "flowbite";
 import "./darkMode.js";
 
-//? Eveything from below will have to be restructured, for not we just test if they work and how it looks
 //> module imports
-import nav from "./nav.js";
+import { getElement } from "./utilis";
+import { toggleDarkMode } from "./darkMode";
+import { getUserName } from "./drawer";
+import nav from "./nav";
+import drawer from "./drawer";
 
-//> Content Page
-const content = document.getElementById("content");
+//! IIFE
 
-//> Append elements
-content.appendChild(nav());
+(function () {
+  //> Content Page
+  const content = document.getElementById("content");
 
-//> DOM elements
-let darkModeToggle = content.querySelector("#toggle");
+  //> Append elements
+  content.appendChild(nav());
+  content.appendChild(drawer());
 
-//> Event listeners
+  //> DOM elements
+  let darkModeToggle = getElement("#toggle");
 
-content.addEventListener("mouseover", (e) => {
-  _handleNavPageHover(e);
-});
-content.addEventListener("mouseout", (e) => {
-  _handleNavPageHover(e);
-});
-document.addEventListener("DOMContentLoaded", () => {
-  _handlePageChangeTxt();
-});
-darkModeToggle.addEventListener("click", toggleDarkMode);
+  //> Event listeners
 
-//> Functions
+  darkModeToggle.addEventListener("click", toggleDarkMode);
 
-let span = document.createElement("span");
-span.className = "font-josefin text-2xl text-gray-700 dark:text-gray-500";
+  //> Functions
+  // getUserName(); //> Will be put back to work as soon as we got things moving better
 
-function _handlePageChangeTxt() {
-  let currentPage = content.querySelector('a[aria-current="page"]');
+  //? this is how we delegate the event listener for the parent element of each drawer option
 
-  if (currentPage) {
-    span.textContent = `${currentPage.getAttribute("data-page")} `;
-    currentPage.insertBefore(span, currentPage.firstChild);
-  }
-}
-
-function _handleNavPageHover(e) {
-  if (e.target === span) return;
-
-  if (e.target.hasAttribute("data-page")) {
-    if (e.type === "mouseover") {
-      span.textContent = `${e.target.getAttribute("data-page")} `;
-      e.target.insertBefore(span, e.target.firstChild);
-    } else if (e.type === "mouseout" && e.relatedTarget !== span) {
-      e.target.removeChild(span);
+  document.addEventListener("click", (e) => {
+    if (e.target.closest("#home")) {
+      console.log("works");
     }
-  }
-}
-
-function toggleDarkMode() {
-  const circle = content.querySelector("#circle");
-  const toggle = content.querySelector("#toggle");
-  const html = document.querySelector("html");
-
-  const isDarkMode = circle.classList.contains("translate-x-5");
-
-  circle.classList.toggle("translate-x-5", !isDarkMode);
-  circle.classList.toggle("bg-black", !isDarkMode);
-  circle.classList.toggle("bg-white", isDarkMode);
-
-  html.classList.toggle("dark", !isDarkMode);
-
-  toggle.classList.toggle("bg-black", isDarkMode);
-  toggle.classList.toggle("bg-white", !isDarkMode);
-}
+  });
+})();
