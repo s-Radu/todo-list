7287c6a0e9934430d726f2cae6dd813818112911
lil.void <tsraduu@gmail.com>
2024-02-05 14:19:08 +0000

    Slight update to home.js

--- src/home.js
+++ src/home.js
@@ -3,7 +3,10 @@ export default function home() {
   home.className = "container mx-auto rounded-2xl font-josefin m-4 home";
   home.innerHTML = `
 <h1 class="text-center text-4xl text-black dark:text-white">Your easiest to do app</h1>
+<p class="text-center text-lg m-4 text-gray-500 dark:text-gray-300">Get started by adding a new task</p>
 `;
 
   return home;
 }
+
+//? Could add another p element that shows how many open tasks are at the moment
