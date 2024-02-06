78da5da5cae84555c77acee9f64af2540839e11a
lil.void <tsraduu@gmail.com>
2024-02-06 20:57:07 +0000

    Improved the look of the projects cards

--- src/completedProjects.js
+++ src/completedProjects.js
@@ -5,9 +5,9 @@ export default function completedProjects() {
   ele.innerHTML = `
   <h1 class="text-center text-4xl m-4 text-gray-500 dark:text-white">Completed Projects</h1>
 
-            <div class="grid grid-cols-autoFill gap-2">
+            <div class="flex flex-wrap justify-center items-start gap-4 p-5 overflow-y-auto scrollbar-hide w-full">
                 <div
-                    class="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-4 flex flex-col justify-between items-center">
+                    class="bg-white dark:bg-gray-800 shadow-lg dark:shadow-sm dark:shadow-white rounded-lg p-4 flex flex-col justify-between items-center max-w-96">
                     <h2 class="text-xl font-semibold m-4 text-gray-500 dark:text-gray-200">Task 1</h2>
                     <p class="text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
                         repellendus mollitia quo rerum fuga! In?</p>
