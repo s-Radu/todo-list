2a93c1d6848e6107ca27d100bfe32f751733286f
lil.void <tsraduu@gmail.com>
2024-02-08 22:38:23 +0000

    modified class names

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
 export default function home() {
   let home = document.createElement("div");
-  home.className = "container mx-auto rounded-2xl font-josefin m-4";
+  home.className = "container mx-auto rounded-2xl m-4";
   home.id = "homePage";
   home.innerHTML = `
 <h1 class="text-center text-4xl text-black dark:text-white">Your easiest to do app</h1>
