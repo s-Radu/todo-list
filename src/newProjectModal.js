1b4c2aec8328f83c2bbc3288f898dd4952dc103e
lil.void <tsraduu@gmail.com>
2024-02-13 21:16:06 +0000

    added data attribute to the button to submit the form

--- src/newProjectModal.js
+++ src/newProjectModal.js
@@ -62,7 +62,7 @@ export default function newProjectModal() {
                             </div>
                         </div>
                         <did class="flex w-full justify-center items-center">
-                            <button
+                            <button data-submit
                                 class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg">
