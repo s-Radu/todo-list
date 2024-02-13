da6a18ca474ef01375c104182c441c294d72b46a
lil.void <tsraduu@gmail.com>
2024-02-13 20:26:55 +0000

    Redone the modal

--- src/newProjectModal.js
+++ src/newProjectModal.js
@@ -1,79 +1,77 @@
-export default function modal() {
+export default function newProjectModal() {
   let modal = document.createElement("div");
-  modal.id = "crud-modal";
-  modal.setAttribute("tabindex", "-1");
-  modal.setAttribute("aria-hidden", "true");
+  modal.setAttribute("data-modal", "");
   modal.className =
     "hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full";
   modal.innerHTML = `
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <div class="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 p-4 w-full max-w-md max-h-full">
 
-                <div class="relative bg-white rounded-lg shadow shadow-black dark:shadow-white dark:bg-gray-700">
+                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 
                     <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                            New Item
+                            Create new todo item
                         </h3>
                         <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                            data-modal-toggle="crud-modal">
-                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 14 14">
+                            data-closeModal>
+                            <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                                     stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                             </svg>
                             <span class="sr-only">Close modal</span>
                         </button>
                     </div>
-                    <form class="p-4 md:p-5" data-newProjectForm>
+
+                    <form class="p-4 md:p-5">
                         <div class="grid gap-4 mb-4 grid-cols-2">
                             <div class="col-span-2">
                                 <label for="name"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                                 <input type="text" name="name" id="name"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    placeholder="eg: groceries" required>
+                                    placeholder="Type item name" required="">
                             </div>
                             <div class="col-span-2 sm:col-span-1">
                                 <label for="date"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Due
-                                    Date</label>
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
                                 <input type="date" name="date" id="date"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    required>
+                                    required="">
                             </div>
                             <div class="col-span-2 sm:col-span-1">
                                 <label for="category"
                                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                                 <select id="category"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                    required>
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option selected="">Select category</option>
                                     <option value="urgent">Urgent</option>
-                                    <option value="mild">Mild</option>
-                                    <option value="meh">Meh</option>
+                                    <option value="important">Important</option>
+                                    <option value="upcoming">Upcoming</option>
+                                    <option value="sometimes">Sometimes</option>
                                 </select>
                             </div>
                             <div class="col-span-2">
                                 <label for="description"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Additional
-                                    information</label>
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
+                                    Description</label>
                                 <textarea id="description" rows="4"
                                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="Do not forget to add any aditional information for the task"></textarea>
+                                    placeholder="Write product description here"></textarea>
                             </div>
                         </div>
-                        <div class="flex w-full items-center justify-center">
-                            <button type="submit" data-submit
-                                class="dark:text-white text-gray-900 inline-flex items-center rounded-lg text-sm px-5 py-2.5 text-center shadow shadow-black dark:shadow-white dark:bg-gray-600 dark:hover:bg-gray-500">
+                        <did class="flex w-full justify-center items-center">
+                            <button type="submit"
+                                class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd"
                                         d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
                                         clip-rule="evenodd"></path>
                                 </svg>
-                                Add new product
+                                Add new list item
                             </button>
-                        </div>
+                        </did>
                     </form>
                 </div>
             </div>
@@ -81,5 +79,4 @@ export default function modal() {
 
   return modal;
 }
-
 //? Might have to get rid of submit button and add a click event listener to the modal to add the new project
