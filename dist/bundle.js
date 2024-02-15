31374a06ea6b0b650694dc2f39d8cb7ed1a98a6c
lil.void <tsraduu@gmail.com>
2024-02-15 20:49:14 +0000

    pushing dist Code working as intended so far

--- dist/bundle.js
+++ dist/bundle.js
@@ -8924,40 +8924,7 @@ function drawer() {
                             </svg>
                         </button>
                         <ul id="dropdown-example" class="hidden py-2 space-y-2">
-                            <li id="newProject-dummy">
-                                <div
-                                    class="cursor-pointer flex items-center justify-between w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 focus:bg-gray-700">Dummy
-                                    <span>
-                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                        </svg>
-                                    </span>
-                                    </div>
-                            </li>
-                            <li id="newProject-test">
-                                <div
-                                    class="cursor-pointer flex items-center justify-between w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 focus:bg-gray-700">Test
-                                    <span>
-                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                        </svg>
-                                    </span>
-                                    </div>
-                            </li>
-                            <li id="newProject-something">
-                                <div
-                                    class="cursor-pointer flex items-center justify-between w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 focus:bg-gray-700">Something
-                                    <span>
-                                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                        </svg>
-                                    </span>
-                                    </div>
-                            </li>
-                            <li id="newProject" data-newProject data-modal-target="crud-modal" data-modal-toggle="crud-modal">
+                            <li id="newProject" data-modal-target="crud-modal" data-modal-toggle="crud-modal">
                                 <div
                                     class="cursor-pointer flex items-center justify-between w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 focus:bg-gray-700">
                                     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="20"
@@ -9530,7 +9497,7 @@ function nav() {
                     </a>
                 </div>
 
-                <div class="flex justify-between items-center w-32 md:mr-4" data-newProject data-modal-target="crud-modal" data-modal-toggle="crud-modal" >
+                <div class="flex justify-between items-center w-32 md:mr-4" data-modal-target="crud-modal" data-modal-toggle="crud-modal" >
                         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40" height="40" class="cursor-pointer hover:scale-105">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -9661,20 +9628,11 @@ function newProjectModal() {
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   addEventListenerToDropdown: () => (/* binding */ addEventListenerToDropdown),
-/* harmony export */   addNewProjectIfClicked: () => (/* binding */ addNewProjectIfClicked),
-/* harmony export */   getIds: () => (/* binding */ getIds),
-/* harmony export */   getNewProjectsIds: () => (/* binding */ getNewProjectsIds)
-/* harmony export */ });
 /* harmony import */ var _utilis__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utilis */ "./src/utilis.js");
-/* harmony import */ var flowbite__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! flowbite */ "./node_modules/flowbite/lib/esm/index.js");
-
 
 
 const DROPDOWN_SELECTOR = "#dropdown-example";
 const NEW_PROJECT_SELECTOR = "#newProject";
-const DATA_NEW_PROJECT = "[data-newProject]";
 
 let ids = [
   "home",
@@ -9686,85 +9644,27 @@ let ids = [
   "newProject",
 ];
 
-let newProjectsIds = [
-  "newProject-something",
-  "newProject-test",
-  "newProject-dummy",
-];
-
-//? Returns a spread out array of ids
-function getIds() {
-  return [...ids];
-}
-
-//? Returns a spread out array of newProjectsIds
-function getNewProjectsIds() {
-  return [...newProjectsIds];
-}
-
-//? Adds delegated event listener to the dropdown so we can have enevt listeners on newly added projects
-function addEventListenerToDropdown() {
-  const parentElement = (0,_utilis__WEBPACK_IMPORTED_MODULE_0__.getElement)(DROPDOWN_SELECTOR);
-
-  if (parentElement) {
-    parentElement.addEventListener("click", handleDropdownClick);
-  }
-}
-
-//? the event listener that will get added to the dropdown list item elements
-function handleDropdownClick(e) {
-  let ele = e.target.closest("li");
-  if (ele) {
-    newProjectsIds.forEach((id) => {
-      if (ele.id === id) {
-        removeNewProject(e);
-      }
-    });
-  }
-}
-
-//? the event listener that will get added to the new project list item elements
-function addNewProjectIfClicked(e) {
-  e.stopPropagation();
-
-  const modalEl = (0,_utilis__WEBPACK_IMPORTED_MODULE_0__.getElement)("#crud-modal");
-  const modal = new flowbite__WEBPACK_IMPORTED_MODULE_1__.Modal(modalEl);
-  modal.show();
-
-  //> Modified so we can make use of both buttons to add new projects
-  const ele = e.target.closest(DATA_NEW_PROJECT);
-  if (ele) {
-    createNewProject();
-  }
-}
-
 //? Removes the new project from the list
 function removeNewProject(e) {
   let element = e.target.closest("li");
-  newProjectsIds = newProjectsIds.filter((id) => id !== element.id);
 
+  //* Remove the project from the projects array
+  projects = projects.filter((project) => {
+    //* Convert the project name to a URL-friendly format
+    const projectId = generateNewProjectId(project.name);
+    return projectId !== element.id;
+  });
+  console.log(...projects);
   element.remove();
 }
 
-//? Creates a new project and adds it to the ids array
-function createNewProject() {
-  const newId = generateNewProjectId();
-  const newElement = createNewElement(newId);
-
-  insertNewElement(newElement);
-  newProjectsIds.push(newId);
+//? Generates a new project id based on the name provided in the form
+function generateNewProjectId(name) {
+  return `${name.replace(/\s+/g, "-").toLowerCase()}`;
 }
 
-//? Generates a new project id [ will later be changed into adding the id by the title of the project added by the user]
-function generateNewProjectId() {
-  //> might dispose of it because we need to get the title of the project as the id, or we can modify it to get the title of the project
-  const randomNumber = Math.floor(Math.random() * 1000);
-  return `newProject-${randomNumber}`;
-}
-
-//? Creates a new list item element with an id provided by the above function
-function createNewElement(newId) {
-  const newTitle = generateNewProjectId();
+//? Creates a new list item element with an id and title provided by the form
+function createNewElement(newId, newTitle) {
   const newElement = document.createElement("li");
   newElement.id = newId;
   newElement.innerHTML = generateNewProjectElement(newTitle);
@@ -9794,10 +9694,20 @@ function insertNewElement(newElement) {
 
   parentUl.insertBefore(newElement, newProjectLi);
 }
+function createNewProjectElement(project) {
+  const newId = generateNewProjectId(project.name);
+  const newElement = createNewElement(newId, project.name);
+
+  //! Add an event listener to the new project element
+  newElement.addEventListener("click", removeNewProject);
+
+  return newElement;
+}
+function appendNewProjectElement(newElement) {
+  insertNewElement(newElement);
+}
 
 //? get data from the form
-
-//> The modal works properly now, but something from below is causing it to bug out
 
 let projects = [];
 
@@ -9805,20 +9715,28 @@ function getFormData(e) {
   e.preventDefault();
 
   const modalEl = (0,_utilis__WEBPACK_IMPORTED_MODULE_0__.getElement)("#crud-modal");
-  const modal = new flowbite__WEBPACK_IMPORTED_MODULE_1__.Modal(modalEl);
+  const modal = FlowbiteInstances.getInstance("Modal", "crud-modal");
   const form = modalEl.querySelector("form");
-  const naem = form.querySelector("#name").value;
+  const name = form.querySelector("#name").value;
   const date = form.querySelector("#date").value;
   const category = form.querySelector("#category").value;
   const description = form.querySelector("#description").value;
 
-  console.log(
-    `Name: ${naem}, Date: ${date}, Category: ${category}, Description: ${description}`
-  );
+  if (name === "" || date === "" || category === "") {
+    alert("Please fill in all the fields");
+  } else {
+    const newProject = { name, date, category, description };
+    projects.push(newProject);
+    console.log(...projects);
+    form.reset();
+    modal.hide();
 
-  form.reset();
-  modal.hide();
-  console.log("modal hidden");
+    //? Create a new project element with the data provided in the form
+    const newElement = createNewProjectElement(newProject);
+
+    //* Append the new project element to the DOM
+    appendNewProjectElement(newElement);
+  }
 }
 
 // setTimeout(() => {
@@ -9853,6 +9771,8 @@ const observer = new MutationObserver(callback);
 
 //> Start observing the target node for configured mutations
 observer.observe(parentElement, config);
+
+
 
 
 /***/ }),
@@ -13786,24 +13706,9 @@ __webpack_require__.r(__webpack_exports__);
   darkModeToggle.addEventListener("click", _darkMode_js__WEBPACK_IMPORTED_MODULE_2__.toggleDarkMode);
 
   //? Adds the event listener to both our buttons
-  newProjectElements.forEach((element) => {
-    element.addEventListener("click", _newProjects__WEBPACK_IMPORTED_MODULE_4__.addNewProjectIfClicked);
-  });
 
   //> Functions
   // getUserName(); //> Will be put back to work as soon as we got things moving better
-
-  //? It's a function that attaches an event listener, so we can't have it anywhere else, we keep it below so it can be easier to read
-
-  _newProjects__WEBPACK_IMPORTED_MODULE_4__.getIds().forEach((id) => {
-    const element = (0,_utilis__WEBPACK_IMPORTED_MODULE_3__.getElement)(`#${id}`);
-
-    if (element) {
-      element.addEventListener("click", _newProjects__WEBPACK_IMPORTED_MODULE_4__.addNewProjectIfClicked);
-    }
-  });
-
-  _newProjects__WEBPACK_IMPORTED_MODULE_4__.addEventListenerToDropdown();
 })();
 
 })();
