bef4f8df10ede537f10cbda307d488de42d6d998
lil.void <tsraduu@gmail.com>
2024-02-16 20:00:28 +0000

    added images, Will need to modify the images and especially the left arrow as it's bigger than the right one

--- src/home.js
+++ src/home.js
@@ -1,3 +1,6 @@
+import arrowLeft from "./imgs/arrowLeft.png";
+import arrowRight from "./imgs/arrowRight.png";
+
 export default function home() {
   let home = document.createElement("div");
   home.className = "container mx-auto rounded-2xl m-4";
@@ -5,6 +8,9 @@ export default function home() {
   home.innerHTML = `
 <h1 class="text-center text-4xl text-black dark:text-white">Your easiest to do app</h1>
 <p class="text-center text-lg m-4 text-gray-500 dark:text-gray-300">Get started by adding a new task</p>
+<div class="flex justify-between w-full ">
+<img src="${arrowLeft}" alt="arrow left" class="w-62 h-62 cursor-pointer">
+<img src="${arrowRight}" alt="arrow right" class="w-62 h-62 cursor-pointer">
 `;
 
   return home;
