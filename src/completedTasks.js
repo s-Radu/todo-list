89dd78af9c3de4a528d38999618b7a5c552b7253
lil.void <tsraduu@gmail.com>
2024-02-20 19:20:02 +0000

    small mods

--- src/completedTasks.js
+++ src/completedTasks.js
@@ -6,7 +6,7 @@ export default function completedTasks() {
   ele.innerHTML = `
   <h1 class="text-center text-4xl m-4 text-gray-500 dark:text-white">Completed Tasks</h1>
 
-   <div data-completedProjectsPage class="flex flex-wrap justify-center items-start gap-4 p-5 overflow-y-auto scrollbar-hide w-full"></div>
+   <div data-completedTasksPage class="flex flex-wrap justify-center items-start gap-4 p-5 overflow-y-auto scrollbar-hide w-full"></div>
   `;
 
   return ele;
