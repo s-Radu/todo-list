49366e9f42cb312f7aee3f34a486bf709099c5b5
lil.void <tsraduu@gmail.com>
2024-02-21 20:34:07 +0000

    modified shadow on the task modal

--- src/newTaskModal.js
+++ src/newTaskModal.js
@@ -6,9 +6,9 @@ export default function newTaskModal() {
   modal.className =
     "hidden overflow-y-auto overflow-x-hidden bg-opacity-75 bg-black fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full";
   modal.innerHTML = `
-      <div class="absolute shadow-sm shadow-black dark:shadow-white left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 rounded-lg p-4 w-full max-w-md max-h-full">
+      <div class="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 rounded-lg p-4 w-full max-w-md max-h-full">
   
-                  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                  <div class="relative bg-white rounded-lg shadow-button shadow-white dark:shadow-gray-500 dark:bg-gray-700">
   
                       <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
