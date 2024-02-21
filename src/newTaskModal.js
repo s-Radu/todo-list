10b0e6b57dac9b68b664eb6f5d60d78d4d44bd5b
lil.void <tsraduu@gmail.com>
2024-02-21 20:27:04 +0000

    small updates to new task modal

--- src/newTaskModal.js
+++ src/newTaskModal.js
@@ -12,7 +12,7 @@ export default function newTaskModal() {
   
                       <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                           <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                              Create new todo item
+                              Create new task
                           </h3>
                           <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -26,10 +26,10 @@ export default function newTaskModal() {
                           <div class="grid gap-4 mb-4 grid-cols-2">
                               <div class="col-span-2">
                                   <label for="name"
-                                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task Name</label>
                                   <input type="text" name="name" id="name"
                                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-600 focus:border-gray-400 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 dark:focus:border-gray-600"
-                                      placeholder="Type item name" required="">
+                                      placeholder="Type task name" required="">
                               </div>
                               <div class="col-span-2 sm:col-span-1">
                                   <label for="date"
@@ -52,11 +52,11 @@ export default function newTaskModal() {
                               </div>
                               <div class="col-span-2">
                                   <label for="description"
-                                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product
+                                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Task
                                       Description</label>
                                   <textarea id="description" rows="4"
                                       class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-gray-400 focus:border-gray-400 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-400 dark:focus:border-gray-400"
-                                      placeholder="Write product description here"></textarea>
+                                      placeholder="Write task description here"></textarea>
                               </div>
                           </div>
                           <did class="flex w-full justify-center items-center">
@@ -67,7 +67,7 @@ export default function newTaskModal() {
                                       d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
                                       clip-rule="evenodd"></path>
                               </svg>
-                              Add new list item
+                              Add task
                           </button>
                           </did>
                       </form>
