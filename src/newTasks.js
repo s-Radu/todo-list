f77d6d2bdcd34024a6ed782c397806797bc62b02
lil.void <tsraduu@gmail.com>
2024-02-22 20:38:11 +0000

    modified the space between close button and title'

--- src/newTasks.js
+++ src/newTasks.js
@@ -228,7 +228,7 @@ function createTODOCardElement(
     <div class="flex items-center justify-between mb-4">
     <h5 class="text-2xl font-bold text-gray-900 dark:text-white">${name}</h5>
     <span class="cursor-pointer hover:scale-110" data-delete="${id}">
-        <svg class="w-3 h-3 text-gray-600 dark:text-white" aria-hidden="true"
+        <svg class="ml-4 w-3 h-3 text-gray-600 dark:text-white" aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
