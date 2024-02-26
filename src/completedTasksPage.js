ddc487a9b46777568b5325be43d7ffcbb9620cdf
lil.void <tsraduu@gmail.com>
2024-02-26 21:35:50 +0000

    modified min page size

--- src/completedTasksPage.js
+++ src/completedTasksPage.js
@@ -3,7 +3,7 @@ export default function completedTasks() {
   let page = createElement({
     tag: "div",
     classes:
-      "hidden flex flex-col font-josefin text-white container mx-auto m-4 min-h-full",
+      "hidden flex flex-col font-josefin text-white container mx-auto m-4 min-h-screen",
     id: "completedPage",
     content: `
     <h1 class="text-center text-4xl m-4 text-gray-500 dark:text-white">Completed Tasks</h1>
