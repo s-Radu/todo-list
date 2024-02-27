6e819b02036dd17244d8d7bd462d5593c62f1534
lil.void <tsraduu@gmail.com>
2024-02-27 21:38:31 +0000

    added Id that stays after refresh

--- src/notesPage.js
+++ src/notesPage.js
@@ -45,6 +45,10 @@ export default function notesPage() {
   return page;
 }
 
+function generateNewNoteId() {
+  return crypto.randomUUID();
+}
+
 function appendForm(e) {
   const parentElement = e.target.closest("#allNotes");
   const form = createForm();
@@ -89,6 +93,7 @@ function addNewNote(e) {
   const title = capitulizeFirstLetter(form.querySelector("input").value);
   const content = capitulizeFirstLetter(form.querySelector("textarea").value);
   const date = todaysDate();
+  const id = generateNewNoteId();
 
   if (!title || !content) {
     alert("Please fill in both fields");
@@ -99,12 +104,14 @@ function addNewNote(e) {
     title,
     content,
     date,
+    id,
   });
 
   const newNote = {
     title,
     content,
     date,
+    id,
   };
 
   updateNotesArray(newNote);
@@ -121,12 +128,12 @@ function deleteNote(e) {
   }
 }
 
-function createNewNote({ title, content, date }) {
+function createNewNote({ title, content, date, id }) {
   let note = createElement({
     tag: "div",
     classes: `bg-white dark:bg-gray-800 text-gray-500 hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-150 ease-in-out shadow-lg shadow-rose-500 dark:shadow-rose-600 dark:shadow-sm rounded-lg p-4 m-4 flex flex-col justify-between items-center relative max-w-96`,
     content: `
-    <span class="absolute top-4 right-4 cursor-pointer hover:scale-105 text-gray-400 dark:hover:text-white hover:text-black" data-delete=''>
+    <span class="absolute top-4 right-4 cursor-pointer hover:scale-105 text-gray-400 dark:hover:text-white hover:text-black" data-delete='${id}'>
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
         </svg>
@@ -172,6 +179,7 @@ document.addEventListener("DOMContentLoaded", () => {
       title: note.title,
       content: note.content,
       date: note.date,
+      id: note.id,
     });
     parentElement.appendChild(newNote);
   });
