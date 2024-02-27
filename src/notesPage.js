edde0f090b406430447b19492eee363e16025429
lil.void <tsraduu@gmail.com>
2024-02-27 22:12:27 +0000

    ability to delete the notes implemented

--- src/notesPage.js
+++ src/notesPage.js
@@ -121,19 +121,23 @@ function addNewNote(e) {
   removeForm(e);
 }
 
-function deleteNote(e) {
-  const parentElement = e.target.closest("[data-delete]");
-  if (parentElement) {
-    console.log("Note deleted");
+function deleteNote(e, id) {
+  const deleteButton = e.target.closest("[data-delete]");
+  if (deleteButton) {
+    const parentElement = deleteButton.closest("div");
+    if (parentElement && parentElement.id === id) {
+      parentElement.remove();
+    }
   }
 }
 
 function createNewNote({ title, content, date, id }) {
   let note = createElement({
     tag: "div",
+    id: id,
     classes: `bg-white dark:bg-gray-800 text-gray-500 hover:text-gray-900 dark:hover:text-gray-200 transition-all duration-150 ease-in-out shadow-lg shadow-rose-500 dark:shadow-rose-600 dark:shadow-sm rounded-lg p-4 m-4 flex flex-col justify-between items-center relative max-w-96`,
     content: `
-    <span class="absolute top-4 right-4 cursor-pointer hover:scale-105 text-gray-400 dark:hover:text-white hover:text-black" data-delete='${id}'>
+    <span class="absolute top-4 right-4 cursor-pointer hover:scale-105 text-gray-400 dark:hover:text-white hover:text-black" data-delete>
         <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
         </svg>
@@ -145,7 +149,7 @@ function createNewNote({ title, content, date, id }) {
   });
 
   const deleteBtn = note.querySelector("[data-delete]");
-  deleteBtn.addEventListener("click", deleteNote);
+  deleteBtn.addEventListener("click", (e) => deleteNote(e, id));
 
   return note;
 }
